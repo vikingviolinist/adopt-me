2c34610c943ab62c51e07990875a7eb6d68f0194
vikingviolinist <misopetrik@gmail.com>
2022-05-21 16:54:45 +0200

    :sparkles: Create custom hook

---
+++ src/useBreedList.js
@@ -0,0 +1,29 @@
+import { useCallback, useState, useEffect } from "react";
+
+const localCache = {};
+
+const useBreedList = (animal) => {
+  const [breeds, setBreeds] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+  const requestBreeds = useCallback(async () => {
+    setBreeds([]);
+    setStatus("loading");
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    );
+    const json = await res.json();
+    localCache[animal] = json.breeds || [];
+    setBreeds(localCache[animal]);
+    setStatus("loaded");
+  }, [animal]);
+
+  useEffect(() => {
+    if (!animal) setBreeds([]);
+    else if (localCache[animal]) setBreeds(localCache[animal]);
+    else requestBreeds();
+  }, [animal, requestBreeds]);
+
+  return [breeds, status];
+};
+
+export default useBreedList;
