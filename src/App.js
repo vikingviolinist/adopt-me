79dc36ea715125b516ca62108e38b9c9d4726ec6
vikingviolinist <misopetrik@gmail.com>
2022-05-21 18:28:37 +0200

    :recycle: Move components to separate dir

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { StrictMode } from "react";
+import { useState, StrictMode } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+import { Details, SearchParams } from "./components";
 
 const App = () => {
+  const theme = useState("salmon");
   return (
     <StrictMode>
-      <BrowserRouter>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
