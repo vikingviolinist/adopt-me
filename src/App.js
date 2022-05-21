bb8fe127c84e5b5181317ee008fae4563d037562
vikingviolinist <misopetrik@gmail.com>
2022-05-21 16:49:06 +0200

    :tada: Finish App component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
